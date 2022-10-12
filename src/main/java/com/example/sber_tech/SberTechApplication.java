e55e7a5c166745bb53f5e6ee575c9e0499044505
Vad <vadstorchak@ya.ru>
2022-10-12 19:14:49 +0300

    Initial commit

---
+++ src/main/java/com/example/sber_tech/SberTechApplication.java
@@ -0,0 +1,13 @@
+package com.example.sber_tech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SberTechApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SberTechApplication.class, args);
+    }
+
+}
